d1b48c39bd589c955c4576718c674a6406c82ebe
maxwell carl scott <tallpyramids@gmail.com>
2017-10-13 16:07:41 -0400

    no redirect for now

--- oregrown-script.js
+++ oregrown-script.js
@@ -1,4 +1,3 @@
 
 // use this to add scripts to the page if you need to:
-// similar behavior as an HTTP redirect
-$(location).attr('href', 'http://maxwellcarlscott.com')
+//$(location).attr('href', 'http://maxwellcarlscott.com')
