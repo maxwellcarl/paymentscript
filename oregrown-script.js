048813346bac9e0ce0ed124e09bddc01cbd0c40d
maxwell carl scott <tallpyramids@gmail.com>
2017-10-13 16:03:54 -0400

    removed file and added redirect

--- oregrown-script.js
+++ oregrown-script.js
@@ -1,2 +1,4 @@
 
-$('body').addClass('WTF');
+// use this to add scripts to the page if you need to:
+// similar behavior as an HTTP redirect
+window.location.replace("http://maxwellcarlscott.com");
